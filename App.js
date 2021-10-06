60e8367f2e596e12353a1f0f46a687dc778f177e
dogancancelik <dogancancelik@sabanciuniv.edu>
2021-10-06 22:49:48 +0300

    config update

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <TextInput style={{borderWidth: 1, padding: 10, width: '90%', borderRadius: 10}}
+                       placeholder='E-mail'/>
+            <TextInput style={{borderWidth: 1, padding: 10, width: '90%', borderRadius: 10, marginTop: 20}}
+                       placeholder='Password'/>
+            <TouchableOpacity style={{
+                alignItems: 'center',
+                backgroundColor: '#657cb1',
+                padding: 10,
+                borderRadius: 10,
+                marginTop: 20,
+                width: 100
+            }}>
+                <Text style={{color: 'white'}}>Login</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={{
+                alignItems: 'center',
+                backgroundColor: '#657cb1',
+                padding: 10,
+                borderRadius: 10,
+                marginTop: 20,
+                width: 100
+            }}>
+                <Text style={{color: 'white'}}>Sign Up</Text>
+            </TouchableOpacity>
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
